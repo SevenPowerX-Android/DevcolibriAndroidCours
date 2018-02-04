ab34dd8860da67a348b805bca4b82d1bfd1d8b95
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2018-02-04 15:27:31 +0100

    Первый способ обработчика OnClick

--- app/src/main/java/ua/com/sevenpowerx/android/devcolibriandroidcours/MainActivity.java
+++ app/src/main/java/ua/com/sevenpowerx/android/devcolibriandroidcours/MainActivity.java
@@ -2,12 +2,23 @@ package ua.com.sevenpowerx.android.devcolibriandroidcours;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        textView = findViewById(R.id.textView);
+    }
+
+    public void sayHello(View view) {
+        textView.setText("Привет!!!");
     }
 }
