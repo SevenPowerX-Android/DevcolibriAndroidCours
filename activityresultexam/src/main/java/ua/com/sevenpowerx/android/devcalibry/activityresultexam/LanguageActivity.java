66689d4707eb8c0471388fc66b7d86b40150d2e6
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2018-02-15 07:35:31 +0100

    start leson 10 master branch

---
+++ activityresultexam/src/main/java/ua/com/sevenpowerx/android/devcalibry/activityresultexam/LanguageActivity.java
@@ -0,0 +1,17 @@
+package ua.com.sevenpowerx.android.devcalibry.activityresultexam;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Лавринюк Андрей User notebook acer  on 15.02.2018.
+ */
+
+public class LanguageActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.language_activity);
+    }
+}
